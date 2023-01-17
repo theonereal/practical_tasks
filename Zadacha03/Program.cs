2065cf55a8f18566a7a12944d8313b28362bb61d
theonereal <onereal@bk.ru>
2023-01-17 15:57:21 +0700

    Написал третью задачу

---
+++ Zadacha03/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 1)
+{
+    Console.WriteLine("Число " + num + " является: Нечетным");
+}
+else
+{
+    Console.WriteLine("Число " + num + " является: Четным");
+}
+Console.WriteLine();
