860ed983a2f38a55516e2e37fe21f319f9023347
theonereal <onereal@bk.ru>
2023-01-17 15:37:35 +0700

    Написал первую задачу

---
+++ Zadacha01/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Ввидите первое число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ввидите второе число: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+if (numA > numB)
+{
+    Console.WriteLine("Первое число " + numA + " больше чем второе " + numB);
+}
+else
+{
+    Console.WriteLine("Второе число " + numB + " больше чем первое " + numA);
+}
+
+Console.WriteLine();
