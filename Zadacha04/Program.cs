b38970d6a05056ba4f46035d741e9ad39826b733
theonereal <onereal@bk.ru>
2023-01-17 16:06:25 +0700

    Написал четвертую  задачу

---
+++ Zadacha04/Program.cs
@@ -0,0 +1,22 @@
+﻿int i = 1;
+bool not = true;
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Четные числа от 1 до " + num);
+while (i <= num)
+{
+    if (i % 2 != 1)
+    {
+        Console.Write(i + ", ");
+        not = false;
+    }
+    i++;
+}
+
+if (not)
+{
+    Console.WriteLine("Нет четных чисел");
+}
+Console.WriteLine();
